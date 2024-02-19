c72fa3b4ad8441db51d23fc23f008214dd626dec
fran6 <igwefran6@gmail.com>
2024-02-19 20:27:30 +0100

    third commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 (function () {
     const $ = new Date();
     let day = document.querySelector(".day");
@@ -30,11 +32,15 @@
     date.textContent = `${$.getDate()}/${$.getMonth() + 1}/${$.getFullYear()}`;
 })();
 
+let loading = document.querySelector(".loading")
+let errorMsg = document.querySelector(".error")
+
 async function getForcast(weatherLocation) {
     let APIURL =
         `https://api.weatherapi.com/v1/current.json?key=dee0e6b3c9ef4846b64101151241702&q=` +
         weatherLocation;
 
+loading.style.display = "block"
     const response = await fetch(APIURL, { mode: "cors" });
     if (!response.ok) {
         throw new Err(`HTTP error! status: ${response.status}`);
@@ -44,23 +50,32 @@ async function getForcast(weatherLocation) {
     }
 }
 
-function weatherForecast() {
+function weatherForecast(e) {
     console.clear();
+    e.disabled = true
+    setTimeout(function() {
+      e.disabled = false
+    }, 2500);
     let weatherLocation = document
         .querySelector(".weather-location")
         .textContent.trim();
     getForcast(weatherLocation)
         .then(data => {
             console.log(data);
+            loading.style.display = "none"
             updatePage(data);
         })
         .catch(error => {
+          loading.style.display = "none"
+          errorMsg.style.display = "block"
+          setTimeout(function() {
+            errorMsg.style.display = "none"
+          }, 5000);
             console.error("Error:", error);
         });
 
     function updatePage(data) {
-        document.getElementById("country").textContent =
-            data.location.country;
+        document.getElementById("country").textContent = data.location.country;
         document.getElementById("wind").textContent =
             data.current.wind_kph + " kph";
         document.getElementById("wind-direction").textContent =
@@ -72,5 +87,20 @@ function weatherForecast() {
         document.getElementById("uv").textContent = data.current.uv + " of 11";
         document.getElementById("visibility").textContent =
             data.current.vis_km + " km";
+        document.getElementById("flc").textContent =
+            data.current.feelslike_c;
+        
+        document.getElementById("flf").textContent =
+            data.current.feelslike_f;
+        
+            
+        document.getElementById("x").innerHTML = `                    <p class="text-large temp">
+                        ${Math.floor(parseInt(data.current.temp_c))}°
+                        <span
+                            id="condition"
+                            class="condition text-bold text-normal"
+                            >${data.current.condition.text}</span
+                        >
+                    </p>`
     }
 }
