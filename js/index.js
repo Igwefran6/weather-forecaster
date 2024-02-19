0086a8fd25332348adfd61e8bea5ada6b37efff3
fran6 <igwefran6@gmail.com>
2024-02-19 22:00:30 +0100

    Fourth commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 (function () {
     const $ = new Date();
     let day = document.querySelector(".day");
@@ -32,46 +30,45 @@
     date.textContent = `${$.getDate()}/${$.getMonth() + 1}/${$.getFullYear()}`;
 })();
 
-let loading = document.querySelector(".loading")
-let errorMsg = document.querySelector(".error")
+let loading = document.querySelector(".loading");
+let errorMsg = document.querySelector(".error");
 
 async function getForcast(weatherLocation) {
     let APIURL =
         `https://api.weatherapi.com/v1/current.json?key=dee0e6b3c9ef4846b64101151241702&q=` +
         weatherLocation;
 
-loading.style.display = "block"
-    const response = await fetch(APIURL, { mode: "cors" });
-    if (!response.ok) {
-        throw new Err(`HTTP error! status: ${response.status}`);
-    } else {
-        const data = await response.json();
+    loading.style.display = "block";
+
+    try {
+        let response = await fetch(APIURL, { mode: "cors" });
+        const data = response.json();
         return data;
+    } catch (error) {
+        console.log("Error fetching data:");
     }
 }
 
 function weatherForecast(e) {
-    console.clear();
-    e.disabled = true
-    setTimeout(function() {
-      e.disabled = false
+    e.disabled = true;
+    setTimeout(function () {
+        e.disabled = false;
     }, 2500);
     let weatherLocation = document
         .querySelector(".weather-location")
         .textContent.trim();
     getForcast(weatherLocation)
         .then(data => {
-            console.log(data);
-            loading.style.display = "none"
+            loading.style.display = "none";
             updatePage(data);
         })
         .catch(error => {
-          loading.style.display = "none"
-          errorMsg.style.display = "block"
-          setTimeout(function() {
-            errorMsg.style.display = "none"
-          }, 5000);
-            console.error("Error:", error);
+            loading.style.display = "none";
+            errorMsg.style.display = "block";
+            setTimeout(function () {
+                errorMsg.style.display = "none";
+            }, 5000);
+            console.log("Error");
         });
 
     function updatePage(data) {
@@ -87,20 +84,39 @@ function weatherForecast(e) {
         document.getElementById("uv").textContent = data.current.uv + " of 11";
         document.getElementById("visibility").textContent =
             data.current.vis_km + " km";
-        document.getElementById("flc").textContent =
-            data.current.feelslike_c;
-        
-        document.getElementById("flf").textContent =
-            data.current.feelslike_f;
-        
-            
-        document.getElementById("x").innerHTML = `                    <p class="text-large temp">
+        document.getElementById("flc").textContent = data.current.feelslike_c;
+
+        document.getElementById("flf").textContent = data.current.feelslike_f;
+
+        document.getElementById(
+            "x"
+        ).innerHTML = `                    <p class="text-large temp">
                         ${Math.floor(parseInt(data.current.temp_c))}°
                         <span
                             id="condition"
                             class="condition text-bold text-normal"
                             >${data.current.condition.text}</span
                         >
-                    </p>`
+                    </p>`;
     }
 }
+
+let usrOffline = document.querySelector(".offline");
+let usrOnline = document.querySelector(".online");
+document.querySelector("button").click();
+window.addEventListener("offline", e => {
+    usrOffline.classList.add("top0");
+    usrOnline.classList.remove("top0");
+    setTimeout(function() {
+      usrOffline.classList.remove("top0");
+    }, 2000);
+});
+
+window.addEventListener("online", e => {
+    usrOnline.classList.add("top0");
+    usrOffline.classList.remove("top0");
+    setTimeout(function() {
+      usrOnline.classList.remove("top0");
+    }, 2000);
+    document.querySelector("button").click();
+});
